1b74f8fe86fcdfc9db477a48742ead0093903bf7
LorisOppia <87977853+LorisOppia@users.noreply.github.com>
2021-08-25 14:30:03 +0200

    Add message after location

--- Bot_Parcheggi.py
+++ Bot_Parcheggi.py
@@ -8,26 +8,32 @@ def dist(lat1,lon1, lat2, lon2):
     return 6371 * 2 * m.asin(m.sqrt(m.pow(m.sin((m.radians(lat2) - m.radians(lat1)) / 2), 2) + m.cos(lat1) * m.cos(lat2) * m.pow(m.sin((m.radians(lon2) - m.radians(lon1)) / 2), 2)))
 
 def start(update: Update, context: CallbackContext) -> None:
-    testo = "Benvenuto!!! \nQuesto bot ti permette di trovare il parcheggio più vicino a te. \nInvia la tua posizione."
+    testo = "Benvenuto!!!\nQuesto bot ti permette di trovare il parcheggio più vicino a te.\nInvia la tua posizione."
     context.bot.send_message(chat_id=update.effective_chat.id, text=testo)
 
 def distanza(update: Update, context: CallbackContext) -> None:
-    lat1 = update.message.location.latitude
-    lon1 = update.message.location.longitude
-    d = []
-    for i in range(0,len(coord_x)):
-        d.append(dist(lat1,lon1,coord_x[i],coord_y[i]))
-    e = d[:]        #uso e come copia
-    d.sort()
-    ind = 0
-    for i in range(0,len(d)):
-        if d[0]==e[i]:
-            ind = i
-            break
-    testo = "Il parcheggio più vicino è: " + nome[ind]
-    testo_dist = "\nDistanza: " + str(round(d[0],2)) + " km"
-    context.bot.send_message(chat_id=update.effective_chat.id, text=testo+testo_dist)
-    update.message.reply_location(coord_x[ind], coord_y[ind])
+    try:
+        lat1 = update.message.location.latitude
+        lon1 = update.message.location.longitude
+        d = []
+        for i in range(0,len(coord_x)):
+            d.append(dist(lat1,lon1,coord_x[i],coord_y[i]))
+        e = d[:]
+        d.sort()
+        ind = 0
+        for i in range(0,len(d)):
+            if d[0]==e[i]:
+                ind = i
+                break
+        testo = "Il parcheggio più vicino è: " + nome[ind]
+        testo_dist = "\nDistanza: " + str(round(d[0],2)) + " km"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=testo+testo_dist)
+        update.message.reply_location(coord_x[ind], coord_y[ind])
+        testo = "Per trovare nuovamente il parcheggio più vicino invia la tua posizione."
+        context.bot.send_message(chat_id=update.effective_chat.id, text=testo)
+    except:
+        testo_try = "Attenzione!!!\nInviare solo la posizione attuale.\nInterrompere la condivisione della posizione in tempo reale."
+        context.bot.send_message(chat_id=update.effective_chat.id, text=testo_try)
 
 def echo(update: Update, context: CallbackContext) -> None:
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
@@ -38,18 +44,18 @@ def main():
 
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
-    dispatcher.add_handler(MessageHandler(Filters.location, distanza))    #update.message.reply_location(10,10) metti coordinate del posto e invia
+    dispatcher.add_handler(MessageHandler(Filters.location, distanza))
 
     updater.start_polling()
 
     updater.idle()
 
 con = psycopg2.connect(
-    host="127.0.0.1",       #192.168.20.20 IP locale
-    database="geoapp",
-    user="postgres",
-    password="postgres"
-)
+	host="host",
+	database="database",
+	user="user",
+	password="password"
+       	 )
 
 cur = con.cursor()
 v = []
@@ -57,9 +63,9 @@ coord_x = []
 coord_y = []
 nome = []
 
-cur.execute("select ST_AsGeoJSON(geom) from parcheggi")    #ricava coordinate in GeoJSON
+cur.execute("select ST_AsGeoJSON(geom) from parcheggi_dedicati")
 a = cur.fetchall()
-cur.execute("select nome from parcheggi")    #ricava coordinate in GeoJSON
+cur.execute("select nome from parcheggi_dedicati")
 b = cur.fetchall()
 for i in range(0,len(a)):
     v.append(json.loads(a[i][0]))
